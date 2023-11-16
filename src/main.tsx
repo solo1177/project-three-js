d23303f3d8e17e417cf9afbd62cd492d7df14e08
mitani <nico.programming1011@gmail.com>
2023-11-17 01:12:12 +0900

    ペンギン

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Penguins from './Penguins.tsx'
+import InteractiveParticles from './InteractiveParticles';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+  <Penguins />
   </React.StrictMode>,
 )
